a9243f9504e5896321de10fa0e71f15063dfd424
Trever Hall <treahall@ut.utm.edu>
2021-09-18 16:34:11 -0500

    Installed dependency react-router-dom to fix a compilation error

--- algorithms-webpage/src/App.js
+++ algorithms-webpage/src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
+import "fontsource-roboto";
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import { Menu } from '@material-ui/icons';
+import { AppBar, Container, ThemeProvider, createTheme, Button, Typography, IconButton, Toolbar } from '@material-ui/core'; 
+import { purple, yellow } from '@material-ui/core/colors';
 import './App.css';
+
+const theme = createTheme({
+  typography: {
+      h2: {
+        fontSize: 36
+      }
+  },
+  palette: {
+    primary: {
+      main: purple[400],
+    },
+    secondary: {
+      main: yellow[200],
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <AppBar color="secondary">
+            <Toolbar>
+              <IconButton>
+                <Menu></Menu>
+              </IconButton>
+              <Typography variant="h6" style={{ flex: 1 }} align="left">
+                Sorting Visualizer
+              </Typography>
+              <Button>
+                Login
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </ThemeProvider>
+    </Router>
+    
   );
 }
 
