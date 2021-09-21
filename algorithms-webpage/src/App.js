69e8ad0026c8f69685500cc208440233238667cd
Trever Hall <treahall@ut.utm.edu>
2021-09-20 23:56:59 -0500

    Swapped over to material-ui v5 and added base ResponsiveDrawer component to be further modified

--- algorithms-webpage/src/App.js
+++ algorithms-webpage/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import "fontsource-roboto";
 import {BrowserRouter as Router, Route} from "react-router-dom"
-import { Menu } from '@material-ui/icons';
-import { AppBar, Container, ThemeProvider, createTheme, Button, Typography, IconButton, Toolbar } from '@material-ui/core'; 
-import { purple, yellow } from '@material-ui/core/colors';
+import { Menu } from '@mui/material';
+import { AppBar, Container, ThemeProvider, createTheme, Button,
+   Typography, IconButton, Toolbar } from '@mui/material'; 
+import { purple, yellow } from '@mui/material/colors';
 import './App.css';
+
+import ResponsiveDrawer from "./components/drawer.component";
+import MainBody from './components/mainbody.component';
 
 const theme = createTheme({
   typography: {
@@ -24,26 +28,15 @@ const theme = createTheme({
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <AppBar color="secondary">
-            <Toolbar>
-              <IconButton>
-                <Menu></Menu>
-              </IconButton>
-              <Typography variant="h6" style={{ flex: 1 }} align="left">
-                Sorting Visualizer
-              </Typography>
-              <Button>
-                Login
-              </Button>
-            </Toolbar>
-          </AppBar>
-        </div>
-      </ThemeProvider>
-    </Router>
-    
+    <ResponsiveDrawer>
+      <MainBody>
+
+      </MainBody>
+    </ResponsiveDrawer>
+      
+
+
+   
   );
 }
 
