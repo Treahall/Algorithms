044739b2ea4a8df072821a0cea39e9f942dd4bf4
Trever Hall <treahall@ut.utm.edu>
2021-10-04 15:54:00 -0500

    removing bloat files

--- algorithms-webpage/src/App.js
+++ algorithms-webpage/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import ReactiveDrawer from "./components/drawer.component.js"
 import "fontsource-roboto";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import { Menu } from '@mui/material';
-import { AppBar, Container, ThemeProvider, createTheme, Button,
-   Typography, IconButton, Toolbar } from '@mui/material';
+import {BrowserRouter as Router} from "react-router-dom"
+import {createTheme} from '@mui/material';
 import { purple, yellow } from '@mui/material/colors';
 import './App.css';
-import AlgorithmList from "./components/algorithm-list.js"
 import ResponsiveDrawer from "./components/drawer.component";
 import MainBody from './components/mainbody.component';
-import InsertionSortComponent from "./components/insertion_sort.js";
-import MergeSortComponent from "./components/merge_sort.js";
-import QuickSortComponent from "./components/quick_sort.js";
-import BubbleSortComponent from "./components/bubble_sort.js";
 
 const theme = createTheme({
   typography: {
@@ -40,9 +32,6 @@ function App() {
         </MainBody>
       </ResponsiveDrawer>
     </Router>
-
-
-
   );
 }
 
